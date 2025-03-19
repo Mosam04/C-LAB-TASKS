d95bd9819db54a0e0adcc40a998cc5f88c135b9f
Mosam04 <bsc-com-ne-33-23@unima.ac.mw>
2025-03-19 03:02:00 +0200

    modified LetterExtract.cpp

--- LetterExtract.cpp
+++ LetterExtract.cpp
@@ -14,11 +14,11 @@ int main(){
         "C235",
         "B179"
      };
-     int size = sizeof(codes)/sizeof(codes[0]);
+     int size = sizeof(codes)/sizeof(codes[0]);  // getting the size of an array
      for(int i =0;i<=size;i++){
       if(codes[i][0]=='C'){
-        cout <<codes[i] <<endl;
+        cout <<codes[i] <<endl; //output strings that starts with letter 'C'
       }
      }
-     return 0;
+     return 0; //terminating the program
 }

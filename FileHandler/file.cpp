c8942c5ef209bfd2929dba5f175d00ce3caca304
Mosam04 <bsc-com-ne-33-23@unima.ac.mw>
2025-04-09 00:22:10 +0200

    committing changes

--- FileHandler/file.cpp
+++ FileHandler/file.cpp
@@ -1,25 +1,56 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cctype> // for tolower
+#include <sstream>// getting the stream ,like how cin works
 
 using namespace std;
-
+//method for reversing
+string reverse(string stmt){
+      string reversed="";
+      for(int i = stmt.length()-1;i>=0;i--){
+          reversed +=stmt[i];
+      }
+      return reversed;
+}
 int main() {
     ifstream infile; 
-    string read;
+    string fileData;
+    int vowelCount=0;
+    int wordCount=0;
     
     infile.open("output.txt");  
     
     if (infile.is_open()) {  
-        getline(infile, read);  
-        cout << read << endl;
-        infile.close();
+        getline(infile, fileData);  
+        cout << fileData << endl;
+        for(char ch:fileData){
+         ch=tolower(ch);
+         if(ch == 'a' ||  ch == 'e' || ch == 'i' || ch == 'o' || ch =='u'){
+            vowelCount++;
+         }
+        }
+        cout << " Number of vowels in  the text is :" << vowelCount << '\n';
+        //count words using stringstream
+        stringstream ss(fileData);
+        string word;
+        while(ss>>word){
+         wordCount++;
+        }
+        cout << " number of words :" << wordCount <<endl;
+
+         //reversed statement
+         cout <<" Reversed statement : " << reverse(fileData) << endl;
     }
+    
     else {
         cout << "Can't open file" << endl;
     }
     
     return 0;
 }
+
+
+
 
 

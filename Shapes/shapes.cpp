2579020697cc23b90b239b7b1047b4b00b8e89e4
Mosam04 <bsc-com-ne-33-23@unima.ac.mw>
2025-04-10 03:31:33 +0200

    this is modified to take users inputs

--- Shapes/shapes.cpp
+++ Shapes/shapes.cpp
@@ -1,57 +1,69 @@
 #include <iostream>
 #include "shapes.h"
-//squares
- shapes::Square::Square(){
-  SideLength;
-}
-shapes::Square::Square(double Length){
-    Length=0;
-}
-shapes::Square::~Square(){//destructor
 
-}
-void::shapes::Square::setSideLength(double Length){
-       Length=SideLength;
-}
-double::shapes::Square::getAreaSquare() const{
-    return SideLength*SideLength;
-}
-//squares
-shapes::Triangle::Triangle(){
-    Base,Height;
-}
-shapes::Triangle::Triangle(double Base,double Height){
-   Base =0;
-   Height=0;
-}
-shapes::Triangle::~Triangle(){
-    //destructor
-}
-void::shapes::Triangle::setBase(double B){
-     B=Base;
-}
-void::shapes::Triangle::setHeight(double H){
-   H=Height;
-}
-double::shapes::Triangle::getAreaTriangle() const{
-    return Base *Height;
-}
-/*double::shapes::Triangle::getHeight() const{
-    return Height;
-}*/
-//Circles
-shapes::Circle::Circle(){ //default constructor
-    Radius;
-}
-shapes::Circle::Circle(double radius){//overloaded constructor
-    Radius=0;
-}
-shapes::Circle::~Circle(){
-    //destructor
-}
-void::shapes::Circle::setRadius(double radius){
-      radius=Radius;
-}
-double::shapes::Circle::getAreaCircle() const{
-    return Radius*Radius*3.14;
-}
+namespace shapes {
+
+    // SQUARE
+    Square::Square() {
+        SideLength = 0;
+    }
+
+    Square::Square(double Length) {
+        SideLength = Length;
+    }
+
+    Square::~Square() {}
+
+    void Square::setSideLength(double Length) {
+        SideLength = Length;
+    }
+
+    double Square::getAreaSquare() const {
+        return SideLength * SideLength;
+    }
+
+    // TRIANGLE
+    Triangle::Triangle() {
+        Base = 0;
+        Height = 0;
+    }
+
+    Triangle::Triangle(double B, double H) {
+        Base = B;
+        Height = H;
+    }
+
+    Triangle::~Triangle() {}
+
+    void Triangle::setBase(double B) {
+        Base = B;
+    }
+
+    void Triangle::setHeight(double H) {
+        Height = H;
+    }
+
+    double Triangle::getAreaTriangle() const {
+        return 0.5 * Base * Height;
+    }
+
+    // CIRCLE
+    Circle::Circle() {
+        Radius = 0;
+    }
+
+    Circle::Circle(double r) {
+        Radius = r;
+    }
+
+    Circle::~Circle() {}
+
+    void Circle::setRadius(double r) {
+        Radius = r;
+    }
+
+    double Circle::getAreaCircle() const {
+        return Radius * Radius * 3.14;
+    }
+
+} // end namespace
